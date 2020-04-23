a2d33fb0eeb8856dd8627bc8b76703882151b49c
nicolas azzara <nicoazzara@hotmail.com>
2020-04-23 00:58:50 -0300

    real-NO FAKE first commit

--- Tatoine/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Tatoine.Global_asax" Language="vb" %>
---
+++ Tatooine/App_Start/IOCContainer.cs
@@ -0,0 +1,26 @@
+﻿namespace Tatooine.App_Start
+{
+    using Autofac;
+
+    public static class IOCContainer
+    {
+        private static IContainer container;
+
+        static IOCContainer()
+        {
+            container = CreateContainer();
+        }
+
+        public static T Resolve<T>()
+        {
+            return container.Resolve<T>();
+        }
+
+        private static IContainer CreateContainer()
+        {
+            var contBuilder = new ContainerBuilder();
+
+            return contBuilder.Build();
+        }
+    }
+}
