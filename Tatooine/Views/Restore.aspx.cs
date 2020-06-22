03512f3b0194bb1c9b5b2d4b3b91bb3bc2a92a78
nicolas rojo <nicolas.martin.rojo@gmail.com>
2020-06-21 22:08:58 -0300

    various fixes

--- Tatooine/Views/Restore.aspx.cs
+++ Tatooine/Views/Restore.aspx.cs
@@ -3,6 +3,7 @@ using DAL.Utils;
 using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
 using System;
+using System.Windows.Forms;
 using Tatooine.App_Start;
 using Tatooine.Helpers;
 
@@ -10,15 +11,25 @@ namespace Tatooine.Views
 {
     public partial class Restore : System.Web.UI.Page
     {
-        private IRestoreDao restore= IOCContainer.Resolve<IRestoreDao>();
+        private IRestoreDao restore = IOCContainer.Resolve<IRestoreDao>();
 
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-        
 
-      protected void Button1_Click(object sender, EventArgs e)
+
+        protected void RestoreButton_Click(object sender, EventArgs e)
+        {
+            //var ruta = RutaDestinoTextBox.Text;
+            var ruta = "C:\\Program Files\\Microsoft SQL Server\\MSSQL14.SQLEXPRESS\\MSSQL\\Backup\\Backup";
+            if (string.IsNullOrWhiteSpace(ruta))
+            {
+                this.SendAlert("Debe selecciona una ruta.");
+                return;
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
         {
             string ruta = TextBox1.Text;
 
@@ -29,14 +40,10 @@ namespace Tatooine.Views
                 return;
             }
 
-           
 
             try
             {
-
-               
                 restore.ExecuteRestore(ruta);
-
             }
             catch (Exception ex)
             {
@@ -46,5 +53,11 @@ namespace Tatooine.Views
             }
         }
 
+        protected void FileSystem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog explorerDialog = new System.Windows.Forms.FolderBrowserDialog();
+            explorerDialog.ShowNewFolderButton = false;
+        }
+
     }
-    }
+}
